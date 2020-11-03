e0bbd1dc862e0f221db7e7df89cdd6cc87c53250
Madalena Boavida Guerra <madalena.boavida@icloud.com>
2020-11-03 12:46:15 +0000

    Add Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,26 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # verb 'path', to: 'controller#action'
+  # RULE!!!! cant have the same path for same verb
+
+  # Read
+  # All
+  get 'tasks', to: 'tasks#index'
+  # One
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # Create
+  # 1. GET the task creation form = 1 request
+  get 'tasks/new', to: 'tasks#new'
+  # 2. POST the parameters to create a new task = 1 request
+  post 'tasks', to: 'tasks#create'
+
+  # Update
+  # 1. GET the task update form = 1 request
+  get 'tasks/:id/edit', to: 'tasks#edit'
+  # 2. PATCH parameters to update an existing task = 1 request
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # Delete
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
