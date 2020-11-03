1618fd129513616753f14e50943a2a89a7e6e46e
Madalena Boavida Guerra <madalena.boavida@icloud.com>
2020-11-03 13:24:15 +0000

    Add new Route, new Controller Action + View for Tasks Details Viewing [path:/tasks/:id controller_action: show path_as: task]

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,17 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show]
+
   # GET /tasks
   def index
     @tasks = Task.all
   end
+
+  attr_reader :show
+  # def show
+  # end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
 end
+
